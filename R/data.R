9b05cb8cfefe7d6ddd11721291a360af7448147a
RobWHickman <robwhickman@gmail.com>
2018-06-03 23:52:17 +0100

    added documentation for the election data

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Election data from 5 countries
+#'
+#' A dataset containing the results of 3 elections for parliamentary houses from
+#'  Russia, Australia, Germany, UK and USA. The variables are as follows:
+#'
+#' \itemize{
+#'   \item year. The year of the election (1990-2017)
+#'   \item country. The country the election took place within (Russia, Australia, Germany, UK, USA)
+#'   \item house. The parliamentary house of the election
+#'   \item party_long. The full name of a party which had elected representatives
+#'   \item party_short. The abbreviated name of a party which had elected representatives
+#'   \item seats. The number of seats won by each party
+#'   \item government. Whether or not that party was a part of the government following the election (1, NA)
+#'   \item colour. A hex code indicating the colours of each party
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name election_data
+#' @usage data(election_data)
+#' @format A data frame with 167 rows and 8 variables
+NULL
