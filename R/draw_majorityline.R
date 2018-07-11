f14e46e985018384aedabaaddb75cc0b1cab2188
Zoe Meers <zoe.meers@sydney.edu.au>
2018-07-11 16:31:42 +1000

    change alpha on majority line to 1 instead of 0.5

--- R/draw_majorityline.R
+++ R/draw_majorityline.R
@@ -42,10 +42,10 @@ ggplot_add.majorityLine <- function(object, plot, object_name) {
   if (object$type == "semicircle") {
     if (!object$label){
       plot +
-        ggplot2::geom_segment(aes(y = 0.8, yend = max(plot$data$y)+0.2, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 0.5)
+        ggplot2::geom_segment(aes(y = 0.8, yend = max(plot$data$y)+0.2, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 1)
     } else {
       plot +
-        ggplot2::geom_segment(aes(y = 0.8, yend = max(plot$data$y)+0.2, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 0.5) +
+        ggplot2::geom_segment(aes(y = 0.8, yend = max(plot$data$y)+0.2, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 1) +
         ggplot2::annotate("text", x = 0.85, y = max(plot$data$y) + 0.2, label = paste0(object$n, " seats needed for a majority."))
     }
    
@@ -53,10 +53,10 @@ ggplot_add.majorityLine <- function(object, plot, object_name) {
     if (object$type == "horseshoe") {
       if (!object$label) {
         plot +
-          ggplot2::geom_segment(aes(y = 7.5, yend = 10.5, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 0.5)
+          ggplot2::geom_segment(aes(y = 7.5, yend = 10.5, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 1)
       } else{
         plot +
-          ggplot2::geom_segment(aes(y = 7.5, yend = 10.5, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 0.5) +
+          ggplot2::geom_segment(aes(y = 7.5, yend = 10.5, x = 0, xend = 0), colour = "grey", size = 0.8, linetype = 2, alpha = 1) +
           ggplot2::annotate("text", x = 0, y = 6, label = paste0(object$n, " seats\nneeded for a\nmajority."))
       }
       
@@ -64,10 +64,10 @@ ggplot_add.majorityLine <- function(object, plot, object_name) {
       if (object$type == "opposing_benches") {
         if (!object$label){
           plot +
-            ggplot2::geom_segment(aes(y = y_pos_oppbenches, yend = y_pos_oppbenches, x = min(plot$data$x), xend = max(plot$data$x)), colour = "grey", size = 0.8, linetype = 2, alpha = 0.5)
+            ggplot2::geom_segment(aes(y = y_pos_oppbenches, yend = y_pos_oppbenches, x = min(plot$data$x), xend = max(plot$data$x)), colour = "grey", size = 0.8, linetype = 2, alpha = 1)
         } else{
           plot +
-            ggplot2::geom_segment(aes(y = y_pos_oppbenches, yend = y_pos_oppbenches, x = min(plot$data$x), xend = max(plot$data$x)), colour = "grey", size = 0.8, linetype = 2, alpha = 0.5) +
+            ggplot2::geom_segment(aes(y = y_pos_oppbenches, yend = y_pos_oppbenches, x = min(plot$data$x), xend = max(plot$data$x)), colour = "grey", size = 0.8, linetype = 2, alpha = 1) +
             ggplot2::annotate("text", x = 0.1, y = y_pos_oppbenches - 5, label = paste0(object$n, " seats needed for a majority."), angle = 90)
         }
         
