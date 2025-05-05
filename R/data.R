100289a0f62bfbd0a6729104add30bc831e408c6
Dean Hansen <haneddean@gmail.com>
2025-05-05 15:19:39 -0400

    Update data.R
    Updated the dataset description to account for current and past changes to the dataset including the number of countries, names of countries available, date range of elections and number of dimensions in the dataset.

--- R/data.R
+++ R/data.R
@@ -1,16 +1,16 @@
-#' Election data from 5 countries
+#' Election data from 7 countries
 #'
-#' A dataset containing the results of 3 elections for parliamentary houses from
-#'  Russia, Australia, Germany, UK and USA. The variables are as follows:
+#' A dataset containing election results for parliamentary houses from
+#' Australia, Canada, Chile, Germany, Russia, UK, and USA. The variables are as follows:
 #'
 #' \itemize{
-#'   \item year. The year of the election (1990-2017)
-#'   \item country. The country the election took place within (Russia, Australia, Germany, UK, USA)
+#'   \item year. The year of the election (1990-2021)
+#'   \item country. The country the election took place within (Australia, Canada, Chile, Germany, Russia, UK, and USA)
 #'   \item house. The parliamentary house of the election
 #'   \item party_long. The full name of a party which had elected representatives
 #'   \item party_short. The abbreviated name of a party which had elected representatives
 #'   \item seats. The number of seats won by each party
-#'   \item government. Whether or not that party was a part of the government following the election (1, NA)
+#'   \item government. Whether or not that party was a part of the government following the election (1, 0)
 #'   \item colour. A hex code indicating the colours of each party
 #' }
 #'
@@ -18,5 +18,5 @@
 #' @keywords datasets
 #' @name election_data
 #' @usage data(election_data)
-#' @format A data frame with 167 rows and 8 variables
+#' @format A tibble with 307 rows and 8 variables
 "election_data"
